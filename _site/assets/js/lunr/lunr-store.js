8d4947e3c379917e082ab2342e417eedf2101189
david <dnjdsxor21@gmail.com>
2023-04-01 07:20:37 +0900

    post2

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "[Jekyll] 블로그 포스팅하는 방법",
+        "excerpt":" ","categories": ["Blog"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/blog/post2/",
+        "teaser": null
+      },{
         "title": "Test Post",
         "excerpt":"This post hello test  ","categories": [],
         "tags": ["Post Formats","readability","test"],
