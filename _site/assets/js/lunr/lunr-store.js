4958a44d94797c25a46784dbc9d030c164d0ce2f
david <dnjdsxor21@gmail.com>
2023-04-01 08:01:56 +0900

    hi

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,13 +1,13 @@
 var store = [{
+        "title": "[Test1]",
+        "excerpt":"This post hello test  ","categories": ["Blog"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/blog/post1/",
+        "teaser": null
+      },{
         "title": "[Jekyll] 블로그 포스팅하는 방법",
         "excerpt":" ","categories": ["Blog"],
         "tags": ["Blog","jekyll","Github","Git"],
         "url": "/blog/post2/",
-        "teaser": null
-      },{
-        "title": "Test Post",
-        "excerpt":"This post hello test  ","categories": [],
-        "tags": ["Post Formats","readability","test"],
-        "url": "/post1/",
         "teaser": null
       }]
