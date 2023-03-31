c11533e450a98c59c74db586bebc88ae76644777
david <dnjdsxor21@gmail.com>
2023-04-01 06:55:30 +0900

    setting

---
+++ _pages/about.md
@@ -0,0 +1,13 @@
+---
+permalink: /about/
+title: "About123"
+toc: true  # table of contents
+toc_sticky: true  # 우측 고정
+toc_label: "MYSELF"  # toc제목
+---
+
+![icon](/assets/logo.ico/apple-icon-120x120.png)
+
+# hello
+## hello
+### hello
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Test Post",
+        "excerpt":"This post hello test  ","categories": [],
+        "tags": ["Post Formats","readability","test"],
+        "url": "/post1/",
+        "teaser": null
+      }]
