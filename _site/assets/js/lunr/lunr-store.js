45f7ea30c179665a6dee96ba0d694ab776542b3d
david <dnjdsxor21@gmail.com>
2023-04-07 14:39:01 +0900

    add transformers

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -20,7 +20,7 @@ var store = [{
         "title": "[Transformer] Attention is all you need #0",
         "excerpt":"attention is all you need  attention is all you need  ","categories": ["Paper"],
         "tags": ["transformer","attention"],
-        "url": "/paper/tf1/",
+        "url": "/paper/tf0/",
         "teaser": null
       },{
         "title": "[Numpy] python numpy",
@@ -32,6 +32,36 @@ var store = [{
         "title": "[Transformer] Attention is all you need #1",
         "excerpt":"attention is all you need  attention is all you need  ","categories": ["Paper"],
         "tags": ["transformer","attention","positional_encoding","embedding"],
+        "url": "/paper/tf1/",
+        "teaser": null
+      },{
+        "title": "[RNN, LSTM, GRU] RNN 기반 모델 구조",
+        "excerpt":"RNN (Recurrent Nueral Network)  은닉층에서 나온 결과값이 다시 은닉층으로 돌아가 새로운 입력값과 연산을 하는 구조   시계열 데이터, 텍스트 데이터같은 가변길이의 데이터에 적합한 모델   학습을 통해 각 단어 다음에 나올 단어를 예측   LSTM  hiddne state는 벡터값이고, $y = W*h$   LSTM의 경우 과거 정보를 가져갈지 말지를 정하는 cell state가 존재. 여기서 c, h는 같은 크기의 벡터이다. 따라서 가중치행렬 사이즈는 두가지 종류임.   $x \\rightarrow h$ $h \\rightarrow h$   각각의 연산에서 행렬의 곱셈부분이 inner product인지 element-wise product인지 확인할 필요가 있다.      GRU  cell state와 비슷한 역할을 하는 hidden state 하나만 존재 -&gt; 경량화된 모델      Reference     Pytorch document   https://byumm315.tistory.com/entry/RNN%EC%9D%84-%EC%95%8C%EC%95%84%EB%B4%85%EC%8B%9C%EB%8B%A4   https://ratsgo.github.io/   natural%20language%20processing/2017/03/09/rnnlstm/  ","categories": ["NLP"],
+        "tags": ["RNN","LSTM","GRU"],
+        "url": "/nlp/RNN-LSTM-GRU/",
+        "teaser": null
+      },{
+        "title": "[Transformer] Attention is all you need #2",
+        "excerpt":"attention is all you need  attention is all you need  ","categories": ["Paper"],
+        "tags": ["transformer","attention"],
         "url": "/paper/tf2/",
+        "teaser": null
+      },{
+        "title": "[Transformer] Attention is all you need #3",
+        "excerpt":"attention is all you need  attention is all you need  ","categories": ["Paper"],
+        "tags": ["transformer","attention","MSA"],
+        "url": "/paper/tf3/",
+        "teaser": null
+      },{
+        "title": "[Transformer] Attention is all you need #4",
+        "excerpt":"attention is all you need  attention is all you need  ","categories": ["Paper"],
+        "tags": ["transformer","normalization","batch","feedforward"],
+        "url": "/paper/tf4-copy/",
+        "teaser": null
+      },{
+        "title": "[Transformer] Attention is all you need #5",
+        "excerpt":"attention is all you need  attention is all you need  ","categories": ["Paper"],
+        "tags": ["transformer","github","code","pytorch"],
+        "url": "/paper/tf5/",
         "teaser": null
       }]
